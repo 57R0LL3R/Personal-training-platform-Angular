81564de8f13ca2c358fb423eb57f0960bc37df00
57R0LL3R <drcecontre@gmail.com>
2025-06-26 18:04:12 -0500

    Add login route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ProfileModule } from './views/profile/profile';
+import { Login } from './views/login/login';
 
 export const routes: Routes = [
-    {path:'profile',component:ProfileModule}
+    {path:'profile',component:ProfileModule},
+    {path:'login',component:Login}
 ];
